a43e6db2d95759ab8dc9ffb9b38b48a6fc1f60bd
JuannMaicon <jmaicon308@gmail.com>
2024-10-09 09:09:29 -0300

    TesteBorderLayout

--- Codigos/codigosPi/src/jFrame/TesteGridLayout.java
+++ Codigos/codigosPi/src/jFrame/TesteGridLayout.java
@@ -1,10 +1,15 @@
 package jFrame;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
-public class TesteGridLayout extends JFrame {
+public class TesteGridLayout extends JFrame implements ActionListener{
+	JButton b1,b2,b3,b4,b5,b6;
 	public TesteGridLayout() {
+		
 		setTitle("Teste GridLayout");
 		setSize(300, 150);
 		getContentPane().setBackground(new Color(180, 189, 255));
@@ -24,10 +29,28 @@ public class TesteGridLayout extends JFrame {
 	private JButton criarBotao(String texto) {
 		JButton botao = new JButton(texto);
 		add(botao);
+		botao.addActionListener((ActionListener) this);
 		return botao;
 	}
 
 	public static void main(String[] args) {
 		new TesteGridLayout();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource()==b1)
+			JOptionPane.showMessageDialog(null, b1.getText());
+		else if(e.getSource()==b2)
+			JOptionPane.showMessageDialog(null, b2.getText());
+		else if(e.getSource()==b3)
+			JOptionPane.showMessageDialog(null, b3.getText());
+		else if(e.getSource()==b4)
+			JOptionPane.showMessageDialog(null, b4.getText());
+		else if(e.getSource()==b5)
+			JOptionPane.showMessageDialog(null, b5.getText());
+		else if(e.getSource()==b6)
+			JOptionPane.showMessageDialog(null, b6.getText());
+			
 	}
 }
