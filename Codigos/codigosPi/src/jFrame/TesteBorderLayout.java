6c0a660a79e22194a6bbac9bb0b07a20eaeec39f
JuannMaicon <jmaicon308@gmail.com>
2024-10-09 08:33:11 -0300

    TesteBorderLayout

--- Codigos/codigosPi/src/jFrame/TesteBorderLayout.java
+++ Codigos/codigosPi/src/jFrame/TesteBorderLayout.java
@@ -13,15 +13,17 @@ public class TesteBorderLayout extends JFrame{
 		BorderLayout bl = new BorderLayout(5,5);		
 		c.setLayout(bl);
 		
-		JButton b1 = criarBotao("Botao Norte" , BorderLayout.NORTH);
-		JButton b2 = criarBotao("Botao Sul" , BorderLayout.SOUTH);
-		JButton b3 = criarBotao("Botao Leste", BorderLayout.EAST);
-		JButton b4 = criarBotao("Botao Oeste", BorderLayout.WEST);
-		JButton b5 = criarBotao("Botao Centro", BorderLayout.CENTER );
+		JButton b1 = criarBotao("Botao Norte" , BorderLayout.NORTH,'N');
+		JButton b2 = criarBotao("Botao Sul" , BorderLayout.SOUTH,'S');
+		JButton b3 = criarBotao("Botao Leste", BorderLayout.EAST,'E');
+		JButton b4 = criarBotao("Botao Oeste", BorderLayout.WEST,'W');
+		JButton b5 = criarBotao("Botao Centro", BorderLayout.CENTER,'C' );
 		setVisible(true);		
 	}
 	
-	private JButton criarBotao(String texto, String posicao) {
+	
+
+	private JButton criarBotao(String texto, String posicao, char c) {
 		JButton b1 = new JButton(texto);
 		add(b1, posicao);
 		return b1;
