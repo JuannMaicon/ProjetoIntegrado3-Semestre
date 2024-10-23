e7e39f254f11a95215277d0d379a04af6ea95d3c
JuannMaicon <jmaicon308@gmail.com>
2024-10-23 10:24:39 -0300

    Continuacao da aula BD

---
+++ Codigos/codigosPi/src/continuacaoBD/Pessoa.java
@@ -0,0 +1,43 @@
+package continuacaoBD;
+
+public class Pessoa {
+	private String nome;
+	private String RG;
+
+	public Pessoa(String nome, String RG) {
+		this.setNome(nome);
+		this.setRG(RG);
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setRG(String RG) {
+		this.RG = RG;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getRG() {
+		return RG;
+	}
+
+	// Sobrescrita do método equals da classe Object
+	public boolean equals(Object o) {
+		// Devemos verificar se o objeto "o" eh uma instancia da classe Pessoa
+		if (o instanceof Pessoa) {
+			// Devemos fazer o cast de Object para Pessoa
+			Pessoa p = (Pessoa) o;
+			if (this.getNome().equals(p.getNome()) && this.getRG().equals(p.getRG())) {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
+}
---
+++ Codigos/codigosPi/src/continuacaoBD/TestePessoa.java
@@ -0,0 +1,14 @@
+package continuacaoBD;
+
+public class TestePessoa {
+	public static void main(String args[]) {
+		Pessoa p1 = new Pessoa("Jessica", "752");
+		Pessoa p2 = new Pessoa("Jessica", "752");
+
+		if (p1.equals(p2)) {
+			System.out.println("p1 igual p2 com equals da classe Pessoa");
+		} else {
+			System.out.println("p1 != p2 com equals da classe Pessoa");
+		}
+	}
+}
