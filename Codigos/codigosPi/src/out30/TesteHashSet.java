a0ba5c9d3d4700f0086cdecb80557c7fea1a350c
JuannMaicon <jmaicon308@gmail.com>
2024-10-30 10:48:09 -0300

    out30

---
+++ Codigos/codigosPi/src/out30/TesteHashSet.java
@@ -0,0 +1,24 @@
+package out30;
+
+import java.util.*;
+public class TesteHashSet {
+	public static void main(String[] args) {
+		String nomesLidos[] = lerNomes();
+		
+		HashSet nomes = new HashSet();
+		for (int i = 0; i < nomesLidos.length; i++) {
+			nomes.add(nomesLidos[i]);
+		}
+		
+		Iterator iterator = nomes.iterator();
+		int iCont = 1;
+		String nome;
+		while (iterator.hasNext()){
+		   nome = (String) iterator.next();
+		   System.out.println("nome[" + iCont++ + "] = " + nome);
+		}	}
+
+	public static String[] lerNomes() {
+		String nomes[] = {"Carlos", "Josias","Marcos",  "Armando"};
+		return nomes;
+	}	} 
